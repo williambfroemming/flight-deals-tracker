197357fd0f31025d17dba4fb348ee488f06fa32b
williambfroemming <80553606+williambfroemming@users.noreply.github.com>
2021-07-13 18:15:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
+from notification_manager import NotificationManager
+
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
-# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager
-# classes to achieve the program requirements.
+ORIGIN_CITY_IATA = "LAX"
 
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+    if flight is None:
+        continue
+
+    if flight is not None and flight.price < destination["lowestPrice"]:
+        notification_manager.send_notification(
+            message=f"Low price alert! Only ${flight.price} to fly from {flight.origin_city}-{flight.origin_airport} "
+                    f"to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} "
+                    f"to {flight.return_date}."
+        )
+        if flight.stop_overs > 0:
+            message += f"\n\nFlight has {flight.stop_overs}, iva {flight.via_city}"
